5841d50140afea80a369d9d3bdff0c3477b36106
Erick Mora <erick_3069@hotmail.com>
2019-05-23 15:58:48 -0500

    parcelable

---
+++ 03-android/app/src/main/java/com/example/a03_android/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.example.a03_android
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu
+import android.view.MenuItem
+
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btn_parcelable.setOnClickListener {
+            irAParcelable()
+        }
+        //setSupportActionBar(toolbar)
+
+        /*fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }*/
+    }
+
+    /*override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }*/
+
+
+
+    fun irAParcelable(){
+        val intentExplicito = Intent(
+            this,
+            Parcelable::class.java
+        )
+        val erick = Usuario("Erick", 22, Date(), 12.12)
+        intentExplicito.putExtra("usuario",erick)
+
+        val cachetes = Mascota("Cachetes", erick)
+        intentExplicito.putExtra("mascota", cachetes)
+
+        startActivity(intentExplicito)
+    }
+
+
+}
