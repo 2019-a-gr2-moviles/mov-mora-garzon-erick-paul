4f4c47fd6f7f487e795c5a8e05800a1163f3cb00
ErickMora <erick_3069@hotmail.com>
2019-06-02 23:59:28 -0500

    interfaces examen

---
+++ examen-primer-bim/app/src/main/java/com/example/examen/Actor.kt
@@ -0,0 +1,13 @@
+package com.example.examen
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Actor : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_actor)
+
+    }
+}
