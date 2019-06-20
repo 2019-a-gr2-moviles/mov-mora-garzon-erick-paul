d0c8193847eea8323be469e79b09f3ec01ac855f
ErickMora <erick_3069@hotmail.com>
2019-06-20 12:33:15 -0500

    deber recycler view

---
+++ deberes/recycler-view/twitter/app/src/main/java/com/example/twitter/ReciclerView.kt
@@ -0,0 +1,95 @@
+package com.example.twitter
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.widget.DefaultItemAnimator
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_recycler_view.*
+
+class ReciclerView : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recycler_view)
+        val lista = arrayListOf<Tweet>()
+        lista.add(
+            Tweet(
+                "Alejandro Jodorowsky",
+                "@alejodo",
+                "1d",
+                "Siente que manipulas lo ligero como si fuera pesado y lo pesado como si fuera ligero. \n\n " +
+                        "Cuando deposites un objeto hazlo con la misma delicadeza con que te despides de tu amada.",
+                4,
+                72,
+                370/*,
+                "11:36 a.m. - 18 jun 19"*/
+            )
+        )
+        lista.add(
+            Tweet(
+                "El Comercio",
+                "@elcomercio",
+                "7h",
+                "Cuatro centros de eduación superior del Ecuador aparecen en el reconocido QS World University Rankings de las mejores universidades del mundo. \n " +
+                          "¿Qué institución lidera a escala global?",
+                11,
+                66,
+                37/*,
+                "7:33 p.m. - 19 jun 19"*/
+            )
+        )
+        lista.add(
+            Tweet(
+                "Sandra Sanchez",
+                "@KushiRimay",
+                "14 jun",
+                "My research paper A Method for Accessibility Testing of Web Applications in Agile Environments reached 600 reads on June 12. \n ",
+                1,
+                3,
+                5/*,
+                "11:20 a.m. - 14 jun. 19"*/
+
+            )
+        )
+
+        iniciarRecylerView(lista, this, rv_tweets)
+
+
+    }
+
+    fun iniciarRecylerView(
+        lista: List<Tweet>,
+        actividad: ReciclerView,
+        recycler_view: RecyclerView
+    ) {
+        val adaptadorTweet = AdaptadorTweet(
+            lista,
+            actividad,
+            recycler_view
+        )
+        recycler_view.adapter = adaptadorTweet
+        recycler_view.itemAnimator = DefaultItemAnimator()
+        recycler_view.layoutManager = LinearLayoutManager(actividad)
+
+        adaptadorTweet.notifyDataSetChanged()
+    }
+
+    fun irAMostrarTweet(tweet: Tweet){
+        val intentExplicito = Intent(
+            this,
+            MostrarTweet::class.java
+        )
+
+        intentExplicito.putExtra("tweet",tweet)
+        startActivity(intentExplicito)
+    }
+
+
+    /*fun cambiarNombreTextView(texto: String) {
+        txv_titulo_rv.text = texto
+    }*/
+
+
+}
---
+++ deberes/recycler-view/twitter/app/src/main/java/com/example/twitter/Tweet.kt
@@ -0,0 +1,57 @@
+package com.example.twitter
+
+import android.os.Parcel
+import android.os.Parcelable
+import java.util.*
+
+class Tweet(
+    var autor: String,
+    var nombreDeUsuario: String,
+    var tiempo: String,
+    var contenido: String,
+    var numComentarios: Int,
+    var numRe: Int,
+    var numMeGusta: Int/*,
+    var fecha: String*/
+
+): Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readInt(),
+        parcel.readInt(),
+        parcel.readInt()/*,
+        parcel.readString()*/
+
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(autor)
+        parcel.writeString(nombreDeUsuario)
+        parcel.writeString(tiempo)
+        parcel.writeString(contenido)
+        parcel.writeInt(numComentarios)
+        parcel.writeInt(numRe)
+        parcel.writeInt(numMeGusta)
+        //parcel.writeString(fecha)
+
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : android.os.Parcelable.Creator<Tweet> {
+        override fun createFromParcel(parcel: Parcel): Tweet {
+            return Tweet(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Tweet?> {
+            return arrayOfNulls(size)
+        }
+    }
+
+}
